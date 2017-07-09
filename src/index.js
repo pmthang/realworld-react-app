44281075ff09919094a3095ea18bdd7cfd20d313
xrr2016 <xiaoranran1993@163.com>
2017-07-09 23:03:15 +0800

    :sparkles:

--- src/index.js
+++ src/index.js
@@ -7,5 +7,30 @@ import './index.css';
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const defaultState = {
+  appName: '留言板',
+  articles: null
+}
+
+const rootReducer = (state = defaultState, action) => {
+  return state
+}
+
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
+
+
+
+
+
+
+
+
+
+
